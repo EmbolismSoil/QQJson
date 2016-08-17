ab0a068545a7f13aec513f93fc3fbedcc8b746d1
EmbolismSoil <lika52090@gmail.com>
2016-08-17 22:54:17 +0800

    添加文件测试，修复小bug

--- example/main.cpp
+++ example/main.cpp
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    std::string str("{\"array\": [1,{\"obj\": 128}]}");
-    auto encoder = QQJsonEncoder::fromString(str);
+    std::string str("test.json");
+    auto encoder = QQJsonEncoder::fromFile(str);
     auto json = encoder->encode();
     std::cout << json->toString() << std::endl;
     return 0;
