6bf4af315a9fa01cc7a9c1ffc30a1ceef23af545
EmbolismSoil <lika52090@gmail.com>
2016-08-18 23:53:35 +0800

    修改示例

--- example/main.cpp
+++ example/main.cpp
@@ -9,14 +9,17 @@ int main(void)
     std::string str("test.json");
     auto encoder = QQJsonEncoder::fromFile(str);
     auto json = encoder->encode();
+
     auto obj = ptr2obj<QQJsonObject>(json); 
     auto sonList = ptr2obj<QQJsonArray>(obj["song_list"]); 
-    std::cout << sonList[2]->toString() << std::endl;
+    auto song = ptr2obj<QQJsonObject>(sonList[2]);
+    std::cout << "artist_name : " << song["artist_name"]->toString() << std::endl;
+
     QQJsonArray array;
     auto object = std::make_shared<QQJsonObject>();    
     (*object)["key"] = std::make_shared<QQJsonString>("value");
     array.push_back(std::make_shared<QQJsonString>("hello world"));
     array.push_back(object);
-    std::cout << array.toString() << std::endl;
+    std::cout << "array : " << array.toString() << std::endl;
     return 0;
 }
